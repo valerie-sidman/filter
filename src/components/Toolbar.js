adae4f0d96d9f6a31ddcf12fb258b58b195b5091
Valerie Sidman <valerie.sidman@gmail.com>
2021-02-22 10:58:41 +0300

    fixed filter

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -7,7 +7,7 @@ export default function Toolbar({filters, selected, onSelectFilter}) {
       {
         filters.map(filter => 
           <li className="tool" key={filter}>
-            <button className="btn-tool" onClick={evt => onSelectFilter(evt.target.textContent)}>{filter}</button>
+            <button className="btn-tool" onClick={() => onSelectFilter(filter)}>{filter}</button>
           </li>)
       }
     </ul>
